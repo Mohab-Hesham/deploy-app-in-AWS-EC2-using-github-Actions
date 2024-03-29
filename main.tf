67a1d3cdcdeba75ebd820f040f1cf4f07cafe287
mohab <mohabhesham3863@gmail.com>
2024-03-29 17:15:04 +0200

    Initial commit

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@
 resource "aws_instance" "app_instance" {
   ami           = "ami-0c02fb55956c7d316"  # Use an appropriate AMI for your region
   instance_type = "t2.micro"
+  key_name = "mohabkey"
  # key_name      = aws_key_pair.deployer_key.key_name
 
   tags = {
